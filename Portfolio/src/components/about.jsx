9e0b5946cc5bd397f81a6be1f0f68d093cdac9dd
Camille Michel <cammichel@sfr.fr>
2024-11-19 21:02:39 +0100

    Finalisation section A Propos en timeline

--- Portfolio/src/components/about.jsx
+++ Portfolio/src/components/about.jsx
@@ -2,25 +2,97 @@ import '../style/components/about.scss';
 
 const About = () => {
     return (
-        <section id="about" className='about'>
-            <div>
-                <h2>A PROPOS DE MOI</h2>
-                <div className='about-box'>
-                    <h3>OPENCLASSROOM</h3>
-                    <p>Diplôme Développement Web</p>
+        <section id="about" className="about">
+            <h2>A PROPOS</h2>
+            <div className="timeline">
+                <div className="timeline-item">
+                    <div className="timeline-point"></div>
+                    <div className="timeline-card top">
+                        <h3>2014 - 2016</h3>
+                        <h4>ISCOM</h4>
+                        <p>BTS Communication</p>
+                        <div className="missions">
+                            <ul>
+                                <li>Stratégie de communication</li>
+                                <li>Création de supports</li>
+                                <li>Relations commerciales</li>
+                                <li>Culture de communication</li>
+                                <li>Organisation événementielle</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div className='about-box'>
-                    <h3>BTS COMMUNICATION</h3>
-                    <p>Diplôme Développement Web</p>
+                <div className="timeline-item">
+                    <div className="timeline-point"></div>
+                    <div className="timeline-card bottom">
+                        <h3>2016 - 2020</h3>
+                        <h4>OLYMPIQUE LYONNAIS</h4>
+                        <p>Service Client - Billetterie</p>
+                        <div className="missions">
+                            <ul>
+                                <li>Responsable clients PSH</li>
+                                <li>Gestion litiges</li>
+                                <li>Adaptabilité événementielle</li>
+                                <li>Vente billets et abonnements</li>
+                                <li>Gestion comptes clients</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <h4>Mais aussi...</h4>
-                <div className='about-box'>
-                    <h3>EXPERIENCE</h3>
-                    <p>7 ans d&apos;expérience en Service Client</p>
+                <div className="timeline-item">
+                    <div className="timeline-point"></div>
+                    <div className="timeline-card top">
+                        <h3>2021</h3>
+                        <h4>LCL</h4>
+                        <p>Expert Banque et Assurance</p>
+                        <div className="missions">
+                            <ul>
+                                <li>Ouverture comptes en ligne</li>
+                                <li>Vérifications identitaires</li>
+                                <li>Conseils banque et assurance</li>
+                                <li>Conseils solutions digitales</li>
+                                <li>Prospection</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="timeline-item">
+                    <div className="timeline-point"></div>
+                    <div className="timeline-card bottom">
+                        <h3>2021 - 2023</h3>
+                        <h4>GOSTUDENT</h4>
+                        <p>Customer Service Manager</p>
+                        <div className="missions">
+                            <ul>
+                                <li>Responsable recherche tuteur</li>
+                                <li>Suivi demandes cours en ligne</li>
+                                <li>Gestion comptes clients</li>
+                                <li>Gestion litiges clients/tuteurs</li>
+                                <li>Rétention</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="timeline-item">
+                    <div className="timeline-point"></div>
+                    <div className="timeline-card top">
+                        <h3>2024</h3>
+                        <h4>OPENCLASSROOM</h4>
+                        <p>Diplôme Développeur Web</p>
+                        <div className="missions">
+                            <ul>
+                                <li>HTML/CSS/JavaScript</li>
+                                <li>Font-End - React.js</li>
+                                <li>Back-End - Express de Node.js</li>
+                                <li>Optimisation et débugging</li>
+                                <li>Gestion de projet</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
-    )
+    );
 };
 
 export default About;
