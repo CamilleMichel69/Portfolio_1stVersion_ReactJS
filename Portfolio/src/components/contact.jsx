c45cfcf7bf8aad35ed57c49509833bf9496c0aec
Camille Michel <cammichel@sfr.fr>
2024-11-19 23:17:35 +0100

    Finalisation description et formulaire de contact avec l'ajout des boutons GitHub et LinkedIn

--- Portfolio/src/components/contact.jsx
+++ Portfolio/src/components/contact.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import emailjs from "emailjs-com";
 import "../style/components/contact.scss"; 
 
@@ -19,19 +21,18 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Vous envoyez les données du formulaire à EmailJS avec les noms des variables qui correspondent à celles définies dans votre template
     const templateParams = {
       from_name: formData.name,
-      from_email: formData.email,  // L'adresse e-mail de la personne qui soumet le formulaire
+      from_email: formData.email, 
       message: formData.message,
     };
 
     emailjs
       .send(
-        "service_zh1a7ul",  // Votre ID de service EmailJS
-        "template_cp0rsbc",  // Votre ID de template EmailJS
-        templateParams,  // Les données que vous voulez envoyer à EmailJS
-        "bqYdXovBltjP2pF_6"  // Votre clé API publique EmailJS
+        "service_zh1a7ul", 
+        "template_cp0rsbc",  
+        templateParams, 
+        "bqYdXovBltjP2pF_6"  
       )
       .then(
         (response) => {
@@ -46,46 +47,59 @@ const Contact = () => {
   };
 
   return (
-    <div id="contact" className="contact-form-container">
-      <h2>CONTACT</h2>
-      {successMessage && <p className="success-message">{successMessage}</p>}
-      <form onSubmit={handleSubmit} className="contact-form">
-        <label htmlFor="name">Nom :</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
+    <div className="contact">
+      <div id="contact" className="contact-form-container">
+        <h2>CONTACT</h2>
+        {successMessage && <p className="success-message">{successMessage}</p>}
+        <form onSubmit={handleSubmit} className="contact-form">
+          <label htmlFor="name">Nom :</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
 
-        <label htmlFor="email">Email :</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
+          <label htmlFor="email">Email :</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
 
-        <label htmlFor="message">Message :</label>
-        <textarea
-          id="message"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          rows="5"
-          required
-        ></textarea>
+          <label htmlFor="message">Message :</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            rows="5"
+            required
+          ></textarea>
 
-        <div>
-        <button type="submit" className="submit-button">
-          Envoyer
-        </button>
+          <div>
+          <button type="submit" className="submit-button">
+            Envoyer
+          </button>
+          </div>
+        </form>
+      </div>
+      <div className="social-box">
+        <p>Retrouvez-moi également sur GitHub et LinkedIn</p>
+        <div className="social-buttons">
+            <a href="https://github.com/CamilleMichel69" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} className="social-icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/camille-michel-a4364aa0/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} className="social-icon" />
+            </a>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
--- Portfolio/src/components/description.jsx
+++ Portfolio/src/components/description.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import Picture from '../images/profile-pic.png';
 import'../style/components/description.scss';
 
@@ -7,7 +10,19 @@ const Description = () => {
             <img className="__img" src={Picture} alt="Photo de Camille MICHEL" />
             <div className="__paragraphe">
                 <h1>Camille Michel</h1>
-                <p>Concevoir, coder et innover pour un web meilleur.</p>
+                <h2>Concevoir, coder et innover pour un web meilleur.</h2>
+                <p>Après 7 années passées à écouter et accompagner les utilisateurs, je me suis naturellement tournée vers le développement web pour concevoir des solutions à leur image.</p>
+                <div className="social-buttons">
+                    <a href="https://github.com/CamilleMichel69" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faGithub} className="social-icon" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/camille-michel-a4364aa0/" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faLinkedin} className="social-icon" />
+                    </a>
+                    <a href="#contact">
+                        <FontAwesomeIcon icon={faEnvelope} className="icon" />
+                    </a>
+                </div>
             </div>
         </section>
     )
